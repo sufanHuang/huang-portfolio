a8d6581d75aabc8f49b68681828f6829d801907f
sufanHuang <sufanhk@gmail.com>
2019-06-08 16:16:06 -0500

    change for deployment

--- config/website.js
+++ config/website.js
@@ -1,16 +1,20 @@
 const tailwind = require('../tailwind')
 
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'Sufan Huang', // Navigation and Site Title
   siteTitleAlt: 'Sufan', // Alternative Site title for SEO
   siteTitleShort: 'Sufan', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteUrl: '', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  author: '', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '', // Twitter Username
+  ogSiteName: 'Sufan', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-47519312-5',
 
