8052054a61cc13e76ae973163b5590d5ba6e369e
sufanHuang <sufanhk@gmail.com>
2019-06-08 15:14:48 -0500

    change README.md

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -57,79 +57,131 @@ const Footer = styled.footer`
   ${tw`text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg`};
 `
 
+
 const Index = () => (
   <>
     <Layout />
     <Parallax pages={5}>
       <Hero offset={0}>
+        <Avatar src={avatar} alt="sufan" />
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Hello, <br /> My Name is Sufan Huang.
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>I am a fullStack programmer.</Subtitle>
+        <hr/>
+        <Title>Skills</Title>
+        <AboutDesc>
+          <h3>FRONTEND:</h3>
+          <h4>HTML CSS Javascript React Gatsby</h4>
+          <h3>BACKEND:</h3>
+          <h4>Node.js Express Serverless SQL Firebase</h4>
+          <h3>OTHERS:</h3>
+          <h4>Git APIs AWS IntelliJ CircleCi</h4>
+        </AboutDesc>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
-            link="https://www.behance.net/gallery/58937147/Freiheit"
+            title="Weather App"
+            link="https://github.com/sufanHuang/weather-react-app"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+            This is my first project to learn about retrieving data from APIs. I use react/bootstrap/weather APIs.
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
+            title="Recipe Search"
+            link="https://github.com/sufanHuang/recipe-app"
             bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+            This app helps me to solididy my experience with API as well as learning about react-router. I use react/bootstrap/API.
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
-            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+            title="Todo APP"
+            link="https://github.com/sufanHuang/todo-app"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
+            This is my first project to connect a frontend react UI with backend mySQL database using node and Express.
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
-            link="https://www.behance.net/gallery/38068151/Eagle"
+            title="Reading-log App"
+            link="https://github.com/sufanHuang/reading-log-app"
             bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+            An app that allows users to add, modify, delete the books the read, using react, bootstrap, firebase as backend.
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
       <About offset={3}>
         <Title>About</Title>
+          <h1 className="text-primary">I HAVE WORKED AS:</h1>
+        <AboutDesc>
+          <div>
+            <div className="card-body">
+              <h6 className="text-primary">Self Taught Programmer (2018-present)</h6>
+              <ul>
+                <li>Study programming through Udemy, Freecodecamp, various Youtube channels.</li>
+                <li>Participate in local tech meet ups.</li>
+                <li>Communicate with small business owners to build websites.</li>
+              </ul>
+              <h6 className="text-primary">Teacher- Des Moines Public School District (2012-2018)</h6>
+              <ul>
+                <li>Develop and maintain program websites using weebly,wordpress,FTP server technologies.</li>
+                <li>Develop Prek-5 Chinese language and culture curriculum for IB school.</li>
+                <li>Develop static web page contents using Storyline software. </li>
+                <li>Provide 6-10 classes daily.</li>
+                <li>Provide formative and summative assessments based on student learning.</li>
+                <li> Communicate with parents for both academy learning and behavior.</li>
+              </ul>
+
+              <h6 className="text-primary">Curriculum Developer-University of Kansas 2010-2012</h6>
+              <ul>
+                <li> Use virtual technology to deliver curriculum and instruction.</li>
+                <li> Create and implement effective and rigorous Club program.</li>
+              </ul>
+
+              <h6 className="text-primary">Administrative Assistant - ZTE (USA), Inc. KS  2010 </h6>
+              <ul>
+                <li>Provide daily administrative services.</li>
+                <li>Compile monthly financial report.</li>
+                <li>Coordinate meetings & recruiting.</li>
+              </ul>
+            </div>
+          </div>
+        </AboutDesc>
+          <hr/>
         <AboutHero>
-          <Avatar src={avatar} alt="John Doe" />
-          <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
-          </AboutSub>
+          <h1>EDUCATION</h1>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+          <ul>
+            <li>Iowa and Kansas K-12 Teaching License (Math/ESL/Chinese)  2012 </li>
+            <li> MA in Education   - University of Kansas  2010</li>
+            <li>BA in English - Shenzhen University, China  2008  </li>
+          </ul>
+        </AboutDesc>
+        <hr/>
+        <AboutHero>
+          <h1>HONORS</h1>
+        </AboutHero>
+        <AboutDesc>
+          <ul>
+            <li>Case Speaker - International Law Moot Court Competition, Washington, D.C.  2007</li>
+            <li>1st Prize Award Winner - International Law Competition,  China    2007 </li>
+          </ul>
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Email me at sufanhk@gmail.com or find me on other platforms:{' '}
+            <a href="https://github.com/sufanHuang">Github</a> &{' '}
+            <a href="https://www.linkedin.com/in/sufan-huang-1829301b/">LinkedIn</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          &copy; 2019 by Sufan Huang Using Gatsby Starter Portfolio{' '}
         </Footer>
       </Contact>
     </Parallax>
