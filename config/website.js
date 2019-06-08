2c3d67ce4ab361a7811657b619942fc11ef2303e
sufanHuang <sufanhk@gmail.com>
2019-06-08 16:28:38 -0500

    change for deployment

--- config/website.js
+++ config/website.js
@@ -4,17 +4,18 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'Sufan Huang', // Navigation and Site Title
-  siteTitleAlt: 'Sufan', // Alternative Site title for SEO
-  siteTitleShort: 'Sufan', // short_name for manifest
+  siteTitleAlt: 'Cara', // Alternative Site title for SEO
+  siteTitleShort: 'Cara', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: '', // Domain of your site. No trailing slash!
+  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'Sufan', // Author for schema.org JSONLD
+  author: 'LekoArts', // Author for schema.org JSONLD
 
-  userTwitter: '@Sufan', // Twitter Username
-  ogSiteName: 'Sufan', // Facebook Site Name
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@cara', // Twitter Username
+  ogSiteName: 'cara', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-47519312-5',
 
